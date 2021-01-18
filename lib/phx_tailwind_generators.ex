dba4675a65ae0ef6d3a9fde62ca4fea40883744b
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2021-01-18 11:35:33 +0100

    Helper function tailwind_error_tag/2

--- lib/phx_tailwind_generators.ex
+++ lib/phx_tailwind_generators.ex
@@ -2,4 +2,14 @@ defmodule PhxTailwindGenerators do
   @moduledoc """
   Documentation for `PhxTailwindGenerators`.
   """
+
+  def tailwind_error_tag(form, field) do
+    Enum.map(Keyword.get_values(form.errors, field), fn error ->
+      content_tag(:p, translate_error(error),
+        class: "mt-2 text-sm text-red-500",
+        phx_feedback_for: input_name(form, field)
+      )
+    end)
+  end
+
 end
