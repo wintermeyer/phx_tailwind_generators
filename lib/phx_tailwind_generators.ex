28e99e6ab668b070ace40fa43b6979e0c3a1efbe
Andreas Eriksson <andreas@codered.se>
2021-01-27 14:44:43 +0100

    Add tailwind error tag

--- lib/phx_tailwind_generators.ex
+++ lib/phx_tailwind_generators.ex
@@ -2,4 +2,23 @@ defmodule PhxTailwindGenerators do
   @moduledoc """
   Documentation for `PhxTailwindGenerators`.
   """
+
+  @doc """
+  Injects snippet before the final end in a file
+  """
+  @spec inject_before_final_end(String.t(), String.t()) :: {:ok, String.t()} | :already_injected
+  def inject_before_final_end(code, code_to_inject) when is_binary(code) and is_binary(code_to_inject) do
+    if String.contains?(code, code_to_inject) do
+      :already_injected
+    else
+      new_code =
+        code
+        |> String.trim_trailing()
+        |> String.trim_trailing("end")
+        |> Kernel.<>(code_to_inject)
+        |> Kernel.<>("end\n")
+
+      {:ok, new_code}
+    end
+  end
 end
