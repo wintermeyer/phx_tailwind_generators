c3a56fcb099cbd288d9fed575e2e43f15e2b03c8
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2021-10-15 14:51:56 +0200

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhxTailwindGenerators.MixProject do
   def project do
     [
       app: :phx_tailwind_generators,
-      version: "0.1.6",
+      version: "0.1.7",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
